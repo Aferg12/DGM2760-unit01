e4ee44cac47228ce3e46585edcd59c153beb56e2
Abigail <10770253@my.uvu.edu>
2021-11-01 14:01:16 -0600

    Initial Commit

---
+++ JavaScript/main.js
@@ -0,0 +1,10 @@
+
+document.getElementById("company").innerHTML = "Joe's Bed and Breakfast"
+
+document.querySelector('header > h2').innerText ="Your New Getaway!"
+
+let username = prompt("What is your name?")
+
+let message = "Hello " + username + ", Welcome to your new favorite getaway!"
+
+document.querySelector("#greeting").innerText = message
